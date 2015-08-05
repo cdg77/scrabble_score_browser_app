57a09b4a0feb263b3094cb712a26c91e8e4cbf55
Toma Apostolov & Carlo De Gregorio <student@epicodus.com>
2015-08-05 14:19:00 -0700

    scrabble works.  It takes a word and returns a score in the browser.

--- app.rb
+++ app.rb
@@ -7,3 +7,8 @@ also_reload('lib/**/*.rb')
 get('/') do
   erb(:index)
 end
+
+get('/scrabble') do
+  @scrabble = params.fetch('scrabble').scrabble()
+  erb(:scrabble)
+end
--- spec/scrabble_integration_spec.rb
+++ spec/scrabble_integration_spec.rb
@@ -2,3 +2,12 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('the scrabble path', {:type => :feature}) do
+  it('processes the user entry and returns the score') do
+    visit('/')
+    fill_in('title', :with => 'ABRACADABRA')
+    click_button('Send')
+    expect(page).to have_content(18)
+  end
+end
